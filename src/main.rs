0706a3f385f2acd6c35bcb37d28ecb13a8cf1d10
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-12-09 23:17:01 +0100

    wip: add reply in read_customer

--- src/main.rs
+++ src/main.rs
@@ -53,8 +53,19 @@ async fn main() {
     .await;
 }
 
-fn read_customer(name: String) -> String {
-    format!("Hello, {}!", name)
+async fn read_customer(name: String) -> Result<impl Reply, Infallible> {
+
+    let code = StatusCode::METHOD_NOT_ALLOWED;
+
+    let json = warp::reply::json(&Customer {
+        email: String::from("test@aaa.de"),
+        first_name: todo!(),
+        last_name: todo!(),
+        date_of_birth: todo!(),
+        addresses: todo!(),
+    });
+
+    Ok(warp::reply::with_status(json, code))
 }
 
 async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
@@ -65,8 +76,6 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
         code = StatusCode::NOT_FOUND;
         message = "NOT_FOUND";
     } else if let Some(e) = err.find::<warp::filters::body::BodyDeserializeError>() {
-        // This error happens if the body could not be deserialized correctly
-        // We can use the cause to analyze the error and customize the error message
         message = match e.source() {
             Some(cause) => {
                 if cause.to_string().contains("denom") {
@@ -79,12 +88,9 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
         };
         code = StatusCode::BAD_REQUEST;
     } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
-        // We can handle a specific error, here METHOD_NOT_ALLOWED,
-        // and render it however we want
         code = StatusCode::METHOD_NOT_ALLOWED;
         message = "METHOD_NOT_ALLOWED";
     } else {
-        // We should have expected this... Just log and say its a 500
         eprintln!("unhandled rejection: {:?}", err);
         code = StatusCode::INTERNAL_SERVER_ERROR;
         message = "UNHANDLED_REJECTION";
